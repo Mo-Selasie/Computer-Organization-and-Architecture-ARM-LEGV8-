2093b4f9eadde2d36e59bfe96934290e3f97a939
Mo-Selasie <153629461+Mo-Selasie@users.noreply.github.com>
2023-12-20 10:59:54 -0500

    Update pc.vhd

--- pc.vhd
+++ pc.vhd
@@ -2,7 +2,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity PC is -- 32-bit rising-edge triggered register with write-enable and synchronous reset
--- For more information on what the PC does, see page 251 in the textbook
 port(
      clk          : in  STD_LOGIC; -- Propogate AddressIn to AddressOut on rising edge of clock
      write_enable : in  STD_LOGIC; -- Only write if '1'
