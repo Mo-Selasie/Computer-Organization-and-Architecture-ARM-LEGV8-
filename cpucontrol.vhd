3e11f1a7167c4bed615521cf1ac00ef7c5aa4a33
Mo-Selasie <153629461+Mo-Selasie@users.noreply.github.com>
2023-12-20 11:11:04 -0500

    Update cpucontrol.vhd

--- cpucontrol.vhd
+++ cpucontrol.vhd
@@ -3,12 +3,6 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity CPUControl is
--- Functionality should match the truth table shown in Figure 4.22 of the textbook, inlcuding the
---    output 'X' values.
--- The truth table in Figure 4.22 omits the unconditional branch instruction:
---    UBranch = '1'
---    MemWrite = RegWrite = '0'
---    all other outputs = 'X'	
 port(Opcode   : in  STD_LOGIC_VECTOR(10 downto 0);
      Reg2Loc   : out STD_LOGIC;
      CBranch  : out STD_LOGIC;  --conditional
