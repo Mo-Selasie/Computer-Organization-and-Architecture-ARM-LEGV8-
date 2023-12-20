7aaf6a69970037c1c50946d0db826fc069a8ffb3
Mo-Selasie <153629461+Mo-Selasie@users.noreply.github.com>
2023-12-20 11:22:04 -0500

    Update wbreg.vhd

--- wbreg.vhd
+++ wbreg.vhd
@@ -1,14 +1,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity WB is -- 32-bit rising-edge triggered register with write-enable and synchronous reset
--- For more information on what the WB does, see page 251 in the textbook
+entity WB is --Register file for memory write back signals
 port(
-     clk          : in  STD_LOGIC; -- Propogate AddressIn to AddressOut on rising edge of clock
+     clk          : in  STD_LOGIC; 
      write_enable : in  STD_LOGIC; -- Only write if '1'
      rst          : in  STD_LOGIC; -- Asynchronous reset! Sets AddressOut to 0x0
-     wb_in    : in  STD_LOGIC_VECTOR(1 downto 0); -- Next WB address
-     wb_out   : out STD_LOGIC_VECTOR(1 downto 0) -- Current WB address
+     wb_in    : in  STD_LOGIC_VECTOR(1 downto 0); -- Next WB signals
+     wb_out   : out STD_LOGIC_VECTOR(1 downto 0) -- Current WB signal
 );
 end WB;
 
@@ -21,15 +20,15 @@ begin
     process (clk, rst)
     begin
         if rst = '1' then
-            WB_Register <= (others => '0'); -- Reset the PC to 0x0 asynchronously
+            WB_Register <= (others => '0'); -- Reset the signals to 0x0 asynchronously
         elsif rising_edge(clk) then
             if write_enable = '1' then
-                WB_Register <= wb_in; -- Write new address when write enable is active
+                WB_Register <= wb_in; 
             end if;
         end if;
     end process;
 
-    -- Output the current PC value
+    -- Output the current signal values
     wb_out <= WB_Register;
 
 end Behavioral;
