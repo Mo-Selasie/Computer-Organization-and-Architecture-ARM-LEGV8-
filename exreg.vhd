547a25badba89cef7570100046710dce2a0cb71d
Mo-Selasie <153629461+Mo-Selasie@users.noreply.github.com>
2023-12-20 11:13:59 -0500

    Update exreg.vhd

--- exreg.vhd
+++ exreg.vhd
@@ -1,8 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity EX is -- 32-bit rising-edge triggered register with write-enable and synchronous reset
--- For more information on what the EX does, see page 251 in the textbook
+entity EX is --Register file for EX stage signals 
 port(
      clk          : in  STD_LOGIC; -- Propogate ex_in to ex_out on rising edge of clock
      write_enable : in  STD_LOGIC; -- Only write if '1'
@@ -15,21 +14,21 @@ end EX;
 
 
 architecture Behavioral of EX is
-    signal EX_Register : STD_LOGIC_VECTOR(2 downto 0); -- PC register
+    signal EX_Register : STD_LOGIC_VECTOR(2 downto 0); -- EX register
 
 begin
     process (clk, rst)
     begin
         if rst = '1' then
-            EX_Register <= (others => '0'); -- Reset the PC to 0x0 asynchronously
+            EX_Register <= (others => '0'); -- Reset the signals to 0x0 asynchronously
         elsif rising_edge(clk) then
             if write_enable = '1' then
-                EX_Register <= ex_in; -- Write new address when write enable is active
+                EX_Register <= ex_in; 
             end if;
         end if;
     end process;
 
-    -- Output the current PC value
+    -- Output the current signal values
     ex_out <= EX_Register;
 
 end Behavioral;
